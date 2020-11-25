193491ae270a31df023923c17e593a4cec76df96
A-Manev <sasho_123_456@abv.bg>
2020-11-25 10:12:25 +0200

    Scaffold Account/Register

---
+++ Web/StayFit.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(StayFit.Web.Areas.Identity.IdentityHostingStartup))]
+
+namespace StayFit.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+            });
+        }
+    }
+}
