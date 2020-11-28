d26d2edcffa06457cfd275448de7a2d120dd653e
A-Manev <sasho_123_456@abv.bg>
2020-11-28 17:11:17 +0200

    Custom attribute

---
+++ Web/StayFit.Web.Infrastructure/CustomAttributes/CurrentYearMaxValueAttribute.cs
@@ -0,0 +1,29 @@
+﻿namespace StayFit.Web.Infrastructure.CustomAttributes
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class CurrentYearMaxValueAttribute : ValidationAttribute
+    {
+        public CurrentYearMaxValueAttribute(int minYear)
+        {
+            this.MinYear = minYear;
+            this.ErrorMessage = $"Year value should be between: {minYear} and {DateTime.UtcNow.Year}.";
+        }
+
+        public int MinYear { get; }
+
+        public override bool IsValid(object value)
+        {
+            if (value is int intValue)
+            {
+                if (intValue <= DateTime.UtcNow.Year && intValue >= this.MinYear)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
