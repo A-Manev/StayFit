76ee8f7abfed0079e7c5450fc2b1cdf6858409ac
A-Manev <sasho_123_456@abv.bg>
2020-11-30 22:51:08 +0200

    Add validations on create meal model form

--- Web/StayFit.Web.ViewModels/Meals/CreateMealViewModel.cs
+++ Web/StayFit.Web.ViewModels/Meals/CreateMealViewModel.cs
@@ -5,57 +5,68 @@
 
     using StayFit.Data.Models.Enums;
     using StayFit.Web.ViewModels.Categories;
-    using StayFit.Web.ViewModels.SubCategories;
 
     public class CreateMealViewModel
     {
-        // [Required]
+        [Required]
         [MinLength(3)]
         public string Name { get; set; }
 
+        [Required]
         public string PreparationTime { get; set; }
 
+        [Required]
         public string CookingTime { get; set; }
 
-        // [Required]
+        [Required]
         [Display(Name = "Skill level")]
         public SkillLevel SkillLevel { get; set; }
 
-        // [Required]
+        [Required]
         public string PortionCount { get; set; }
 
-        // [Required]
+        [Required]
         [Range(0, 2000)]
-        [Display(Name = "Calories")]
+        [Display(Name = "Calories(g)")]
         public double KCal { get; set; }
 
         [Range(0, 200)]
+        [Display(Name = "Fat(g)")]
         public double Fat { get; set; }
 
+        [Display(Name = "Saturates(g)")]
         public double Saturates { get; set; }
 
         [Range(0, 200)]
+        [Display(Name = "Carbs(g)")]
         public double Carbs { get; set; }
 
+        [Display(Name = "Sugars(g)")]
         public double Sugars { get; set; }
 
+        [Display(Name = "Fibre(g)")]
         public double Fibre { get; set; }
 
         [Range(0, 200)]
+        [Display(Name = "Protein(g)")]
         public double Protein { get; set; }
 
+        [Display(Name = "Salt(g)")]
         public double Salt { get; set; }
 
         public string Description { get; set; }
 
-        // [Required]
+        [Required]
+        [MinLength(100)]
         [Display(Name = "Method of preparation step by step")]
         public string MethodOfPreparation { get; set; }
 
+        [Required]
         [Display(Name = "Category")]
         public int CategoryId { get; set; }
 
-        [Display(Name = "Sub Category")]
+        [Required]
+        [Display(Name = "Subcategory")]
         public string SubCategory { get; set; }
 
         public IEnumerable<MealIngredientInputModel> Ingredients { get; set; }
