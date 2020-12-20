13b8d81742f79cadfa80581aac348f25c6b9ecdd
A-Manev <sasho_123_456@abv.bg>
2020-12-20 16:42:06 +0200

    Make some changes on administration area

--- Web/StayFit.Web/Areas/Administration/Controllers/DashboardController.cs
+++ Web/StayFit.Web/Areas/Administration/Controllers/DashboardController.cs
@@ -1,22 +1,16 @@
 ﻿namespace StayFit.Web.Areas.Administration.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
-    using StayFit.Services.Data;
-    using StayFit.Web.ViewModels.Administration.Dashboard;
 
     public class DashboardController : AdministrationController
     {
-        private readonly ISettingsService settingsService;
-
-        public DashboardController(ISettingsService settingsService)
+        public DashboardController()
         {
-            this.settingsService = settingsService;
         }
 
         public IActionResult Index()
         {
-            var viewModel = new IndexViewModel { SettingsCount = this.settingsService.GetCount(), };
-            return this.View(viewModel);
+            return this.View();
         }
     }
 }
